949af6bf8728c5719c6ae537dcd1d50729f368cc
curt <curt6815463@gmail.com>
2018-07-04 02:26:20 +0800

    chore: add agnoster.fish fish_mode_prompt.fish

--- functions/fish_mode_prompt.fish
+++ functions/fish_mode_prompt.fish
@@ -1,1 +1,20 @@
-/Users/curt/.config/fisherman/agnoster/fish_mode_prompt.fish
+function fish_mode_prompt
+  if set -q __fish_vi_mode
+    switch $fish_bind_mode
+      case insert
+        set_color green
+        echo I
+      case visual
+        set_color magenta
+        echo V
+      case replace-one
+        set_color green
+        echo R
+      case default
+        set_color blue
+        echo N
+    end
+
+    set_color normal
+  end
+end
